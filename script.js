1b8b19a0166bd4653f35a186da2a1e2c043dbd20
BogdanovGennadievichRuslan <88258628+BogdanovGennadievichRuslan@users.noreply.github.com>
2021-09-23 08:57:16 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-burgerMenu.onclick = function myFunction() {
-	var x = document.getElementById('menu')
-
-	if (x.className === "menu") {
-		x.className+=" menu-ON";
-	} else{
-		x.className = "menu";
-	}
-}
